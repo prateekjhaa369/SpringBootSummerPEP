955abca2db340d34251d2d59af6aab2bd891dc71
Prateek Jha <151683328+prateekjhaa369@users.noreply.github.com>
2026-07-09 15:47:53 +0530

    Add files via upload

---
+++ SpringBoot-Summer-PEP/Day12/Project1/orderservice/src/main/java/org/example/orderservice/OrderserviceApplication.java
@@ -0,0 +1,13 @@
+package org.example.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderserviceApplication.class, args);
+    }
+
+}
